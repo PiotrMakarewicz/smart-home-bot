60a0d0a6360476420c69c774d33591c2d568ec8e
PiotrMakarewicz <sxpgch@gmail.com>
2021-11-08 17:12:07 +0100

    initial

---
+++ src/test/java/piotrmakarewicz/weatherbotapi/WeatherBotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package piotrmakarewicz.weatherbotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
