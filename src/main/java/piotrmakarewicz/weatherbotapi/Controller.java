c2defe602c1557a07cd9d3f2ccff2aeb55a4383a
PiotrMakarewicz <sxpgch@gmail.com>
2021-11-09 12:53:30 +0100

    change

--- src/main/java/piotrmakarewicz/weatherbotapi/Controller.java
+++ src/main/java/piotrmakarewicz/weatherbotapi/Controller.java
@@ -38,7 +38,7 @@ public class Controller {
         GoogleCloudDialogflowV2WebhookRequest request = jacksonFactory.createJsonParser(rawRequest)
                 .parse(GoogleCloudDialogflowV2WebhookRequest.class);
 
-        System.out.println("INTENT IS:" + request.getOriginalDetectIntentRequest());
+        System.out.println("RECEIVED REQUEST: " + request);
 
         StringWriter stringWriter = new StringWriter();
         var jsonGenerator = jacksonFactory.createJsonGenerator(stringWriter);
